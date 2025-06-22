7ca96a3393c91e80ea59564ffc44e92ba1742731
Emu76 <emu76@hotmail.co.uk>
2025-06-22 16:08:32 +0100

    [Feature] Add ioDispatcher

--- data/src/main/java/com/peteremo/data/repository/ToDoRepositoryImpl.kt
+++ data/src/main/java/com/peteremo/data/repository/ToDoRepositoryImpl.kt
@@ -1,21 +1,28 @@
 package com.peteremo.data.repository
 
 import com.peteremo.data.api.ToDoApi
+import com.peteremo.data.di.IoDispatcher
 import com.peteremo.data.extensions.NetworkExtensions.check
 import com.peteremo.domain.home.ToDoRepository
 import com.peteremo.domain.home.model.ToDoDomainModel
 import dagger.Reusable
+import kotlinx.coroutines.CoroutineDispatcher
 import javax.inject.Inject
 
 @Reusable
 class ToDoRepositoryImpl @Inject constructor(
-    private val api: ToDoApi
+    private val api: ToDoApi,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
 ) : ToDoRepository {
     override suspend fun getTodos(): List<ToDoDomainModel>? {
-        return api.getAllTodos().check()
+        with(ioDispatcher) {
+            return api.getAllTodos().check()
+        }
     }
 
     override suspend fun getTodoByKey(key: String): ToDoDomainModel? {
-        return api.getTodo(key).check()
+        with(ioDispatcher) {
+            return api.getTodo(key).check()
+        }
     }
 }
